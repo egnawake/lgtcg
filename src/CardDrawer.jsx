17634fcf104b7a1fd07d9ed43cc925cccafa1cad
João Inácio <egnawake@gmail.com>
2025-12-10 01:47:32 +0000

    Implement weighted draws

--- src/CardDrawer.jsx
+++ src/CardDrawer.jsx
@@ -1,10 +1,31 @@
 import cardData from "./card-data.json";
 
+function rollRarity(chances) {
+  const weightTotal = chances
+    .map((c) => c.weight)
+    .reduce((sum, current) => sum + current, 0);
+
+  const sample = Math.random() * weightTotal;
+
+  let sum = 0;
+  for (const c of chances) {
+    sum = sum + c.weight;
+    if (sample < sum) return c;
+  }
+}
+
+function chooseCard(chances, cards) {
+  const rarity = rollRarity(chances);
+  const cardsWithRarity = cards.filter((c) => c.rarity === rarity.id);
+  const i = Math.floor(Math.random() * cardsWithRarity.length);
+  return cardsWithRarity[i];
+}
+
 function drawCards(data) {
   const cards = [];
   for (let i = 0; i < 3; i++) {
-    const id = Math.floor(Math.random() * data.cards.length);
-    cards.push(data.cards[id]);
+    const card = chooseCard(data.chances, data.cards);
+    cards.push(card);
   }
 
   return cards;
